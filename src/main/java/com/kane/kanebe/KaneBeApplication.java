b39aac246359b57a2f5a8ff7e1b72bf3926a5a65
이명진 <myeongjin@82203.local>
2023-02-14 14:55:21 +0900

    Initial commit

---
+++ src/main/java/com/kane/kanebe/KaneBeApplication.java
@@ -0,0 +1,13 @@
+package com.kane.kanebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaneBeApplication.class, args);
+    }
+
+}
